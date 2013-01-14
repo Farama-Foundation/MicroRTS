1146798378460febc82b227756fa0a725e2ae119
santi.ontanon@gmail.com <santi.ontanon@gmail.com@cc6e48ba-01b3-ca0b-d6ad-4ba6df87cdd9>
2013-01-14 04:44:18 +0000

    Just some refactoring

---
+++ src/ai/mcts/uct/ContinuingUCT.java
@@ -0,0 +1,216 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai.mcts.uct;
+
+import ai.montecarlo.*;
+import ai.AI;
+import ai.RandomBiasedAI;
+import ai.evaluation.EvaluationFunction;
+import ai.evaluation.SimpleEvaluationFunction;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import rts.GameState;
+import rts.PlayerAction;
+import rts.PlayerActionGenerator;
+
+/**
+ *
+ * @author santi
+ */
+public class ContinuingUCT extends AI {
+    public static int DEBUG = 0;
+    EvaluationFunction ef = null;
+       
+    Random r = new Random();
+    AI randomAI = new RandomBiasedAI();
+    long max_actions_so_far = 0;
+    
+    GameState gs_to_start_from = null;
+    UCTNode tree = null;
+    
+    // statistics:
+    public long total_runs = 0;
+    public long total_cycles_executed = 0;
+    public long total_actions_issued = 0;
+    
+    long total_runs_this_move = 0;
+        
+    int TIME_PER_CYCLE = 100;
+    int MAXSIMULATIONTIME = 1024;
+    int MAX_TREE_DEPTH = 10;
+    
+    
+    public ContinuingUCT(int available_time, int lookahead, int max_depth, AI policy, EvaluationFunction a_ef) {
+        MAXSIMULATIONTIME = lookahead;
+        randomAI = policy;
+        TIME_PER_CYCLE = available_time;
+        MAX_TREE_DEPTH = max_depth;
+        ef = a_ef;
+    }
+    
+    
+    public void printStats() {
+        if (total_cycles_executed>0 && total_actions_issued>0) {
+            System.out.println("Average runs per cycle: " + ((double)total_runs)/total_cycles_executed);
+            System.out.println("Average runs per action: " + ((double)total_runs)/total_actions_issued);
+        }
+    }
+    
+    
+    public void reset() {
+        gs_to_start_from = null;
+        tree = null;
+        total_runs_this_move = 0;
+    }
+    
+    
+    public AI clone() {
+        return new ContinuingUCT(TIME_PER_CYCLE, MAXSIMULATIONTIME, MAX_TREE_DEPTH, randomAI, ef);
+    }  
+    
+    
+    public PlayerAction getAction(int player, GameState gs) throws Exception {
+        if (gs.winner()!=-1) return new PlayerAction();
+        if (gs.canExecuteAnyAction(player)) {
+            // continue or start a search:
+            if (tree==null) {
+                startNewSearch(player,gs);
+            } else {
+                if (!gs.getPhysicalGameState().equivalents(gs_to_start_from.getPhysicalGameState())) {
+                    System.err.println("Game state used for search NOT equivalent to the actual one!!!");
+                    System.err.println("gs:");
+                    System.err.println(gs);
+                    System.err.println("gs_to_start_from:");
+                    System.err.println(gs_to_start_from);
+                }
+            }
+            search(player, TIME_PER_CYCLE);
+            PlayerAction best = getBestAction();
+            resetSearch();
+            return best;
+        } else {
+            if (tree!=null) {
+                // continue previous search:
+                search(player, TIME_PER_CYCLE);
+            } else {
+                // determine who will be the next player:
+                GameState gs2 = gs.clone();
+                while(gs2.winner()==-1 && 
+                      !gs2.gameover() &&  
+                    !gs2.canExecuteAnyAction(0) && 
+                    !gs2.canExecuteAnyAction(1)) gs2.cycle();
+                if (gs2.canExecuteAnyAction(player)) {
+                    // start a new search:
+                    startNewSearch(player,gs2);
+                    search(player, TIME_PER_CYCLE);
+                    return new PlayerAction();
+                } else {
+                    return new PlayerAction();
+                }
+            }
+        }
+        
+        return new PlayerAction();
+    }    
+    
+    public void startNewSearch(int player, GameState gs) throws Exception {
+        float evaluation_bound = SimpleEvaluationFunction.upperBound(gs);
+        tree = new UCTNode(player, 1-player, gs, null, evaluation_bound);
+        gs_to_start_from = gs;
+        total_runs_this_move = 0;
+//        System.out.println(evaluation_bound);
+    }    
+    
+    
+    public void resetSearch() {
+        if (DEBUG>=2) System.out.println("Resetting search...");
+        tree = null;
+        gs_to_start_from = null;
+        total_runs_this_move = 0;
+    }
+    
+
+    public void search(int player, long available_time) throws Exception {
+        if (DEBUG>=2) System.out.println("Search...");
+        long start = System.currentTimeMillis();
+        long cutOffTime = start + available_time;
+
+//        System.out.println(start + " + " + available_time + " = " + cutOffTime);
+        
+        while(System.currentTimeMillis() < cutOffTime) {
+            UCTNode leaf = tree.UCTSelectLeaf(player, 1-player, cutOffTime, MAX_TREE_DEPTH);
+            
+            if (leaf!=null) {
+                GameState gs2 = leaf.gs.clone();
+                simulate(gs2, gs2.getTime() + MAXSIMULATIONTIME);
+                
+                int time = gs2.getTime() - gs_to_start_from.getTime();
+                double evaluation = ef.evaluate(player, 1-player, gs2)*Math.pow(0.99,time/10.0);
+            
+//                System.out.println(evaluation_bound + " -> " + evaluation + " -> " + (evaluation+evaluation_bound)/(evaluation_bound*2));
+                
+                while(leaf!=null) {
+                    leaf.accum_evaluation += evaluation;
+                    leaf.visit_count++;
+                    leaf = leaf.parent;
+                }
+                total_runs++;
+                total_runs_this_move++;
+            } else {
+                // no actions to choose from :)
+                System.err.println(this.getClass().getSimpleName() + ": claims there are no more leafs to explore...");
+                break;
+            }
+        }
+        
+        total_cycles_executed++;
+    }
+    
+    
+    public PlayerAction getBestAction() {
+        total_actions_issued++;
+                
+        int mostVisitedIdx = -1;
+        UCTNode mostVisited = null;
+        for(int i = 0;i<tree.children.size();i++) {
+            UCTNode child = tree.children.get(i);
+            if (mostVisited == null || child.visit_count>mostVisited.visit_count) {
+                mostVisited = child;
+                mostVisitedIdx = i;
+            }
+        }
+        
+//        tree.showNode(0,0);
+        if (DEBUG>=2) tree.showNode(0,1);
+        if (DEBUG>=1) System.out.println(this.getClass().getSimpleName() + " performed " + total_runs_this_move + " playouts.");
+        if (DEBUG>=1) System.out.println(this.getClass().getSimpleName() + " selected children " + tree.actions.get(mostVisitedIdx) + " explored " + mostVisited.visit_count + " Avg evaluation: " + (mostVisited.accum_evaluation/((double)mostVisited.visit_count)));
+        
+//        printStats();   
+        
+        if (mostVisitedIdx==-1) return new PlayerAction();
+        
+        return tree.actions.get(mostVisitedIdx);
+    }
+    
+    
+    public void simulate(GameState gs, int time) throws Exception {
+        boolean gameover = false;
+
+        do{
+            if (gs.isComplete()) {
+                gameover = gs.cycle();
+            } else {
+                gs.issue(randomAI.getAction(0, gs));
+                gs.issue(randomAI.getAction(1, gs));
+            }
+        }while(!gameover && gs.getTime()<time);   
+    }
+    
+    public String toString() {
+        return "ContinuingUCT(" + MAXSIMULATIONTIME + ")";
+    }
+    
+}
---
+++ src/ai/mcts/uct/UCT.java
@@ -0,0 +1,126 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai.mcts.uct;
+
+import ai.*;
+import ai.evaluation.EvaluationFunction;
+import ai.evaluation.SimpleEvaluationFunction;
+import java.util.List;
+import java.util.Random;
+import rts.GameState;
+import rts.PlayerAction;
+
+/**
+ *
+ * @author santi
+ */
+public class UCT extends AI {
+    public static int DEBUG = 1;
+    EvaluationFunction ef = null;
+
+    Random r = new Random();
+    AI randomAI = new RandomAI();
+    int max_actions_so_far = 0;
+    
+    int NSIMULATIONS = 1000;
+    int MAXSIMULATIONTIME = 500;
+    int MAX_TREE_DEPTH = 10;
+
+    public UCT(int simulations, int time, int max_depth, AI policy, EvaluationFunction a_ef) {
+        super();
+        NSIMULATIONS = simulations;
+        MAXSIMULATIONTIME = time;   
+        MAX_TREE_DEPTH = max_depth;
+        randomAI = policy;
+        ef = a_ef;
+    }
+    
+    
+    public void reset() {
+    }
+
+    
+    public AI clone() {
+        return new UCT(NSIMULATIONS, MAXSIMULATIONTIME, MAX_TREE_DEPTH, randomAI, ef);
+    }  
+        
+    
+    public PlayerAction getAction(int player, GameState gs) throws Exception {
+
+        if (gs.canExecuteAnyAction(player) && gs.winner()==-1) {
+            PlayerAction pa = UCT(player, 1-player, gs, NSIMULATIONS, gs.getTime()+MAXSIMULATIONTIME); 
+            return pa;
+        } else {
+            return new PlayerAction();
+        }        
+    }
+    
+    
+    public PlayerAction UCT(int maxplayer, int minplayer, GameState gs, int T, int cutOffTime) throws Exception {
+        float evaluation_bound = SimpleEvaluationFunction.upperBound(gs);
+        UCTNode tree = new UCTNode(maxplayer, minplayer, gs, null, evaluation_bound);
+        
+        if (DEBUG>=1) System.out.println(this.getClass().getSimpleName() + " started...");
+
+        for(int i = 0;i<T;i++) {
+            UCTNode leaf = tree.UCTSelectLeaf(maxplayer, minplayer, -1, MAX_TREE_DEPTH);
+            
+            if (leaf!=null) {
+                GameState gs2 = leaf.gs.clone();
+                simulate(gs2,cutOffTime);
+                
+                int time = gs2.getTime() - gs.getTime();
+                double evaluation = ef.evaluate(maxplayer, minplayer, gs2)*Math.pow(0.99,time/10.0);
+            
+                while(leaf!=null) {
+                    leaf.accum_evaluation += evaluation;
+                    leaf.visit_count++;
+                    leaf = leaf.parent;
+                }
+            }
+
+        }
+        
+        int mostVisitedIdx = -1;
+        UCTNode mostVisited = null;
+        for(int i = 0;i<tree.children.size();i++) {
+            UCTNode child = tree.children.get(i);
+            if (mostVisited == null || child.visit_count>mostVisited.visit_count) {
+                mostVisited = child;
+                mostVisitedIdx = i;
+            }
+        }
+        
+        if (DEBUG>=1) {
+            if (DEBUG>=2) {
+                tree.showNode(0,1);
+            } else {
+                tree.showNode(0,0);                
+            }
+            System.out.println(this.getClass().getSimpleName() + " selected children " + tree.actions.get(mostVisitedIdx) + " explored " + mostVisited.visit_count + " Avg evaluation: " + (mostVisited.accum_evaluation/((double)mostVisited.visit_count)));
+        }
+        
+        if (mostVisitedIdx==-1) return new PlayerAction();
+        
+        return tree.actions.get(mostVisitedIdx);
+    }
+    
+    
+
+    
+    
+    public void simulate(GameState gs, int time) throws Exception {
+        boolean gameover = false;
+
+        do{
+            if (gs.isComplete()) {
+                gameover = gs.cycle();
+            } else {
+                gs.issue(randomAI.getAction(0, gs));
+                gs.issue(randomAI.getAction(1, gs));
+            }
+        }while(!gameover && gs.getTime()<time);   
+    }
+}
