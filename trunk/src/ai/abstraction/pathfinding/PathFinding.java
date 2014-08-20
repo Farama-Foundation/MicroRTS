62a3a90782c7e051bed05f67a629411be85b2b6f
santi.ontanon@gmail.com <santi.ontanon@gmail.com@cc6e48ba-01b3-ca0b-d6ad-4ba6df87cdd9>
2014-08-20 21:39:08 +0000

    added a couple of extra functions to the pathfinding modules

--- trunk/src/ai/abstraction/pathfinding/PathFinding.java
+++ trunk/src/ai/abstraction/pathfinding/PathFinding.java
@@ -13,6 +13,8 @@ import rts.units.Unit;
  * @author santi
  */
 public abstract class PathFinding {
+    public abstract boolean pathExists(Unit start, int targetpos, GameState gs);
+    public abstract boolean pathToPositionInRangeExists(Unit start, int targetpos, int range, GameState gs);
     public abstract UnitAction findPath(Unit start, int targetpos, GameState gs);
     public abstract UnitAction findPathToPositionInRange(Unit start, int targetpos, int range, GameState gs);
     public abstract UnitAction findPathToAdjacentPosition(Unit start, int targetpos, GameState gs);
