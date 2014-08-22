a0b9148f8c1e956d75cbdcb90c3c0ac92345109f
santi.ontanon@gmail.com <santi.ontanon@gmail.com@cc6e48ba-01b3-ca0b-d6ad-4ba6df87cdd9>
2014-08-22 10:33:01 +0000

    added an additional evaluation function

---
+++ src/ai/evaluation/SimpleSqrtEvaluationFunction2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai.evaluation;
+
+import rts.GameState;
+import rts.PhysicalGameState;
+import rts.units.*;
+
+/**
+ *
+ * @author santi
+ */
+public class SimpleSqrtEvaluationFunction2 extends EvaluationFunction {    
+    public static float RESOURCE = 20;
+    public static float RESOURCE_IN_WORKER = 10;
+    public static float UNIT_BONUS_MULTIPLIER = 40.0f;
+    
+    
+    public float evaluate(int maxplayer, int minplayer, GameState gs) {
+        float s1 = base_score(maxplayer,gs);
+        float s2 = base_score(minplayer,gs);
+        if (s1==0 && s2!=0) return -VICTORY;
+        if (s1!=0 && s2==0) return VICTORY;
+        return  s1 - s2;
+    }
+    
+    public float base_score(int player, GameState gs) {
+        PhysicalGameState pgs = gs.getPhysicalGameState();
+        float score = gs.getPlayer(player).getResources()*RESOURCE;
+        boolean anyunit = false;
+        for(Unit u:pgs.getUnits()) {
+            if (u.getPlayer()==player) {
+                anyunit = true;
+                score += u.getResources() * RESOURCE_IN_WORKER;
+                score += UNIT_BONUS_MULTIPLIER * (u.getCost()*Math.sqrt(u.getHitPoints()))/(float)u.getMaxHitPoints();
+            }
+        }
+        if (!anyunit) return 0;
+        return score;
+    }    
+    
+    public static float upperBound(GameState gs) {
+        PhysicalGameState pgs = gs.getPhysicalGameState();
+        int free_resources = 0;
+        int player_resources[] = {gs.getPlayer(0).getResources(),gs.getPlayer(1).getResources()};
+        for(Unit u:pgs.getUnits()) {
+            if (u.getPlayer()==-1) free_resources+=u.getResources();
+            if (u.getPlayer()==0) {
+                player_resources[0] += u.getResources();
+                player_resources[0] += u.getCost();
+            }
+            if (u.getPlayer()==1) {
+                player_resources[1] += u.getResources();
+                player_resources[1] += u.getCost();                
+            }
+        }
+        return (free_resources + Math.max(player_resources[0],player_resources[1]))*UNIT_BONUS_MULTIPLIER;
+    }
+}
