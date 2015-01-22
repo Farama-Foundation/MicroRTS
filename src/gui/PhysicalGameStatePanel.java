6b4655b92a0ee8967a69f5e2f69026e56a5cf0d9
santi.ontanon@gmail.com <santi.ontanon@gmail.com@cc6e48ba-01b3-ca0b-d6ad-4ba6df87cdd9>
2015-01-22 04:43:45 +0000

    changed the physicalstatepanel to allow rendering to an image

--- src/gui/PhysicalGameStatePanel.java
+++ src/gui/PhysicalGameStatePanel.java
@@ -163,15 +163,27 @@ public class PhysicalGameStatePanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D)g;
-        
-        if (pgs==null) return;
-        int gridx = (this.getWidth()-64)/pgs.getWidth();
-        int gridy = (this.getHeight()-64)/pgs.getHeight();
+        synchronized(this) {
+            draw(g2d, this, this.getWidth(), this.getHeight(), gs, pgs, pogs, colorScheme, evalFunction);
+        }
+    }
+    
+    
+    public static void draw(Graphics2D g2d, 
+                            PhysicalGameStatePanel panel, 
+                            int dx,int dy, 
+                            GameState gs,
+                            PhysicalGameState pgs,
+                            PartiallyObservableGameState pogs[],
+                            int colorScheme,
+                            EvaluationFunction evalFunction) {
+        int gridx = (dx-64)/pgs.getWidth();
+        int gridy = (dy-64)/pgs.getHeight();
         int grid = Math.min(gridx,gridy);
         int sizex = grid*pgs.getWidth();
         int sizey = grid*pgs.getHeight();
-        
-        if (pogs[0]!=null && pogs[1]!=null) {
+                
+        if (pogs!=null && pogs[0]!=null && pogs[1]!=null) {
             if (pogs[0].getTime() != gs.getTime()) {
                 // update 
                 pogs[0] = new PartiallyObservableGameState(gs, 0);
@@ -179,8 +191,8 @@ public class PhysicalGameStatePanel extends JPanel {
             }
         }
         
-        if (colorScheme==COLORSCHEME_BLACK) g.setColor(Color.WHITE);
-        if (colorScheme==COLORSCHEME_WHITE) g.setColor(Color.BLACK);
+        if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.WHITE);
+        if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.BLACK);
         
         int unitCount0 = 0;
         for (Unit unit : gs.getPhysicalGameState().getUnits()) {
@@ -195,20 +207,25 @@ public class PhysicalGameStatePanel extends JPanel {
             }
         }
 
-        float eval0 = evalFunction.evaluate(0, 1, gs);
-        float eval1 = evalFunction.evaluate(1, 0, gs);
+        float eval0 = (evalFunction!=null ? evalFunction.evaluate(0, 1, gs):0);
+        float eval1 = (evalFunction!=null ? evalFunction.evaluate(1, 0, gs):0);
 
         String info = "T: " + gs.getTime() + ", P₀: " + unitCount0 + " (" + eval0 + "), P₁: " + unitCount1 + " (" + eval1 + ")";
-        g.drawString(info, 10, getHeight()-15);
+        g2d.drawString(info, 10, dy-15);
         
         
 //        g.drawString(gs.getTime() + "", 10, getHeight()-15);
         
-        last_start_x = getWidth()/2 - sizex/2;
-        last_start_y = getHeight()/2 - sizey/2;
-        last_grid = grid;
-
-        g2d.translate(last_start_x, last_start_y);
+        if (panel!=null) {
+            panel.last_start_x = dx/2 - sizex/2;
+            panel.last_start_y = dy/2 - sizey/2;
+            panel.last_grid = grid;
+            g2d.translate(panel.last_start_x, panel.last_start_y);
+        } else {
+            int last_start_x = dx/2 - sizex/2;
+            int last_start_y = dy/2 - sizey/2;
+            g2d.translate(last_start_x, last_start_y);
+        }
         
         Color playerColor = null;
         Color wallColor = new Color(0, 0.33f, 0);
@@ -218,156 +235,154 @@ public class PhysicalGameStatePanel extends JPanel {
 
         for(int j = 0;j<pgs.getWidth();j++) {
             for(int i = 0;i<pgs.getHeight();i++) {
-                if (pogs[0]!=null && pogs[1]!=null) {
+                if (pogs!=null && pogs[0]!=null && pogs[1]!=null) {
                     // show partial observability:
                     if (pogs[0].observable(j, i)) {
                         if (pogs[1].observable(j, i)) {
-                            g.setColor(pobothcolor);
-                            g.fillRect(j*grid, i*grid, grid, grid);
+                            g2d.setColor(pobothcolor);
+                            g2d.fillRect(j*grid, i*grid, grid, grid);
                         } else {
-                            g.setColor(po0color);
-                            g.fillRect(j*grid, i*grid, grid, grid);
+                            g2d.setColor(po0color);
+                            g2d.fillRect(j*grid, i*grid, grid, grid);
                         }
                     } else {
                         if (pogs[1].observable(j, i)) {
-                            g.setColor(po1color);
-                            g.fillRect(j*grid, i*grid, grid, grid);
+                            g2d.setColor(po1color);
+                            g2d.fillRect(j*grid, i*grid, grid, grid);
                         } else {
                         }
                     }
                 }
 
                 if (pgs.getTerrain(j,i)==PhysicalGameState.TERRAIN_WALL) {
-                    g.setColor(wallColor);
-                    g.fillRect(j*grid, i*grid, grid, grid);
+                    g2d.setColor(wallColor);
+                    g2d.fillRect(j*grid, i*grid, grid, grid);
                 }
             }            
         }
         
         // draw grid:
-        if (colorScheme==COLORSCHEME_BLACK) g.setColor(Color.GRAY);
-        if (colorScheme==COLORSCHEME_WHITE) g.setColor(Color.BLACK);
+        if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.GRAY);
+        if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.BLACK);
         for(int i = 0;i<=pgs.getWidth();i++) 
-            g.drawLine(i*grid, 0, i*grid, pgs.getHeight()*grid);
+            g2d.drawLine(i*grid, 0, i*grid, pgs.getHeight()*grid);
         for(int i = 0;i<=pgs.getHeight();i++) 
-            g.drawLine(0, i*grid, pgs.getWidth()*grid, i*grid);
+            g2d.drawLine(0, i*grid, pgs.getWidth()*grid, i*grid);
         
         // draw the units:
-        synchronized(this) {
-            // this list copy is to prevent a concurrent modification exception
-            List<Unit> l = new LinkedList<Unit>();
-            l.addAll(pgs.getUnits());
-            for(Unit u:l) {
-                int reduction = 0;
+        // this list copy is to prevent a concurrent modification exception
+        List<Unit> l = new LinkedList<Unit>();
+        l.addAll(pgs.getUnits());
+        for(Unit u:l) {
+            int reduction = 0;
 
-                // Draw the action:
-                UnitActionAssignment uaa = gs.getActionAssignment(u);
-                if (uaa!=null) {
-                    int offsx = 0;
-                    int offsy = 0;
-                    if (uaa.action.getType()==UnitAction.TYPE_ATTACK_LOCATION) {
-                        offsx = (uaa.action.getLocationX() - u.getX())*grid;
-                        offsy = (uaa.action.getLocationY() - u.getY())*grid;
-                    } else {
-                        if (uaa.action.getDirection()==UnitAction.DIRECTION_UP) offsy = -grid;
-                        if (uaa.action.getDirection()==UnitAction.DIRECTION_RIGHT) offsx = grid;
-                        if (uaa.action.getDirection()==UnitAction.DIRECTION_DOWN) offsy = grid;
-                        if (uaa.action.getDirection()==UnitAction.DIRECTION_LEFT) offsx = -grid;
-                    }
-                    switch(uaa.action.getType()) {
-                        case UnitAction.TYPE_MOVE:
-                            g.setColor(Color.GRAY);
-                            g.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
-                            break;
-                        case UnitAction.TYPE_ATTACK_LOCATION:
-                            g.setColor(Color.RED);
-                            g.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
-                            break;
-                        case UnitAction.TYPE_PRODUCE:
-                            g.setColor(Color.BLUE);
-                            g.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
-                            break;
-                        case UnitAction.TYPE_HARVEST:
-                        case UnitAction.TYPE_RETURN:
-                            if (colorScheme==COLORSCHEME_BLACK) g.setColor(Color.WHITE);
-                            if (colorScheme==COLORSCHEME_WHITE) g.setColor(Color.GREEN);
-                            g.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
-                            break;
-                    }
-                }
-
-                if (u.getPlayer()==0) {
-                    playerColor = Color.blue;
-                } else if (u.getPlayer()==1) {
-                    playerColor = Color.red;
-                } else if (u.getPlayer()==-1) {
-                    playerColor = null;
-                }
-
-                if (u.getType().name.equals("Resource")) {
-                    g.setColor(Color.green);
-                }
-                if (u.getType().name.equals("Base")) {
-                    if (colorScheme==COLORSCHEME_BLACK) g.setColor(Color.white);
-                    if (colorScheme==COLORSCHEME_WHITE) g.setColor(Color.lightGray);
-                }
-                if (u.getType().name.equals("Barracks")) {
-                    if (colorScheme==COLORSCHEME_BLACK) g.setColor(Color.lightGray);
-                    if (colorScheme==COLORSCHEME_WHITE) g.setColor(Color.gray);
-                }
-                if (u.getType().name.equals("Worker")) {
-                    g.setColor(Color.gray);
-                    reduction = grid/4;
-                }
-                if (u.getType().name.equals("Light")) {
-                    g.setColor(Color.orange);
-                    reduction = grid/8;
-                }
-                if (u.getType().name.equals("Heavy")) g.setColor(Color.yellow);
-                if (u.getType().name.equals("Ranged")) {
-                    g.setColor(Color.cyan);
-                    reduction = grid/8;
-                }
-                
-                if (!u.getType().canMove) {
-                    g.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
-                    g.setColor(playerColor);
-                    if (toHighLight.contains(u)) g.setColor(Color.green);
-                    g.drawRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);        
+            // Draw the action:
+            UnitActionAssignment uaa = gs.getActionAssignment(u);
+            if (uaa!=null) {
+                int offsx = 0;
+                int offsy = 0;
+                if (uaa.action.getType()==UnitAction.TYPE_ATTACK_LOCATION) {
+                    offsx = (uaa.action.getLocationX() - u.getX())*grid;
+                    offsy = (uaa.action.getLocationY() - u.getY())*grid;
                 } else {
-                    g.fillOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
-                    g.setColor(playerColor);
-                    if (toHighLight.contains(u)) g.setColor(Color.green);
-                    g.drawOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);        
+                    if (uaa.action.getDirection()==UnitAction.DIRECTION_UP) offsy = -grid;
+                    if (uaa.action.getDirection()==UnitAction.DIRECTION_RIGHT) offsx = grid;
+                    if (uaa.action.getDirection()==UnitAction.DIRECTION_DOWN) offsy = grid;
+                    if (uaa.action.getDirection()==UnitAction.DIRECTION_LEFT) offsx = -grid;
                 }
-
-                if (u.getType().isStockpile) {
-                    // print the player resources in the base:
-                    String txt = "" + pgs.getPlayer(u.getPlayer()).getResources();
-                    g.setColor(Color.black);
-
-                    FontMetrics fm = getFontMetrics( g.getFont() );
-                    int width = fm.stringWidth(txt);
-                    g2d.drawString(txt, u.getX()*grid + grid/2 - width/2, u.getY()*grid + grid/2);
-                }
-
-
-                if (u.getResources()!=0) {
-                    String txt = "" + u.getResources();
-                    g.setColor(Color.black);
-
-                    FontMetrics fm = getFontMetrics( g.getFont() );
-                    int width = fm.stringWidth(txt);
-                    g2d.drawString(txt, u.getX()*grid + grid/2 - width/2, u.getY()*grid + grid/2);
-                }
-                
-                if (u.getHitPoints()<u.getMaxHitPoints()) {
-                    g.setColor(Color.RED);
-                    g.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid, 2);
-                    g.setColor(Color.GREEN);
-                    g.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, (int)(grid*(((float)u.getHitPoints())/u.getMaxHitPoints())), 2);
+                switch(uaa.action.getType()) {
+                    case UnitAction.TYPE_MOVE:
+                        g2d.setColor(Color.GRAY);
+                        g2d.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
+                        break;
+                    case UnitAction.TYPE_ATTACK_LOCATION:
+                        g2d.setColor(Color.RED);
+                        g2d.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
+                        break;
+                    case UnitAction.TYPE_PRODUCE:
+                        g2d.setColor(Color.BLUE);
+                        g2d.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
+                        break;
+                    case UnitAction.TYPE_HARVEST:
+                    case UnitAction.TYPE_RETURN:
+                        if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.WHITE);
+                        if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.GREEN);
+                        g2d.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
+                        break;
                 }
             }
-        }      
+
+            if (u.getPlayer()==0) {
+                playerColor = Color.blue;
+            } else if (u.getPlayer()==1) {
+                playerColor = Color.red;
+            } else if (u.getPlayer()==-1) {
+                playerColor = null;
+            }
+
+            if (u.getType().name.equals("Resource")) {
+                g2d.setColor(Color.green);
+            }
+            if (u.getType().name.equals("Base")) {
+                if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.white);
+                if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.lightGray);
+            }
+            if (u.getType().name.equals("Barracks")) {
+                if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.lightGray);
+                if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.gray);
+            }
+            if (u.getType().name.equals("Worker")) {
+                g2d.setColor(Color.gray);
+                reduction = grid/4;
+            }
+            if (u.getType().name.equals("Light")) {
+                g2d.setColor(Color.orange);
+                reduction = grid/8;
+            }
+            if (u.getType().name.equals("Heavy")) g2d.setColor(Color.yellow);
+            if (u.getType().name.equals("Ranged")) {
+                g2d.setColor(Color.cyan);
+                reduction = grid/8;
+            }
+
+            if (!u.getType().canMove) {
+                g2d.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
+                g2d.setColor(playerColor);
+                if (panel!=null && panel.toHighLight.contains(u)) g2d.setColor(Color.green);
+                g2d.drawRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);        
+            } else {
+                g2d.fillOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
+                g2d.setColor(playerColor);
+                if (panel!=null && panel.toHighLight.contains(u)) g2d.setColor(Color.green);
+                g2d.drawOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);        
+            }
+
+            if (u.getType().isStockpile) {
+                // print the player resources in the base:
+                String txt = "" + pgs.getPlayer(u.getPlayer()).getResources();
+                g2d.setColor(Color.black);
+
+                FontMetrics fm = g2d.getFontMetrics( g2d.getFont() );
+                int width = fm.stringWidth(txt);
+                g2d.drawString(txt, u.getX()*grid + grid/2 - width/2, u.getY()*grid + grid/2);
+            }
+
+
+            if (u.getResources()!=0) {
+                String txt = "" + u.getResources();
+                g2d.setColor(Color.black);
+
+                FontMetrics fm = g2d.getFontMetrics( g2d.getFont() );
+                int width = fm.stringWidth(txt);
+                g2d.drawString(txt, u.getX()*grid + grid/2 - width/2, u.getY()*grid + grid/2);
+            }
+
+            if (u.getHitPoints()<u.getMaxHitPoints()) {
+                g2d.setColor(Color.RED);
+                g2d.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid, 2);
+                g2d.setColor(Color.GREEN);
+                g2d.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, (int)(grid*(((float)u.getHitPoints())/u.getMaxHitPoints())), 2);
+            }
+        }
     }    
 }
