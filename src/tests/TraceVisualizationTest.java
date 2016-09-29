85164a9f2a8e2442d63e7481085cc4d6ba54c65c
Nicolas A. Barriga <nbarriga@gmail.com>
2016-09-29 13:46:12 -0600

    Trace visualizer can now open zipped traces.

--- src/tests/TraceVisualizationTest.java
+++ src/tests/TraceVisualizationTest.java
@@ -5,7 +5,11 @@
 package tests;
 
 import gui.TraceVisualizer;
+
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.zip.ZipInputStream;
+
 import javax.swing.*;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
@@ -19,9 +23,24 @@ import rts.units.UnitTypeTable;
 public class TraceVisualizationTest {
 
   public static void main(String []args) throws JDOMException, IOException, Exception {
-      UnitTypeTable utt = new UnitTypeTable();
-      Trace t = new Trace(new SAXBuilder().build("trace.xml").getRootElement(), utt);
-      JFrame tv = TraceVisualizer.newWindow("Demo", 800, 600, t, 1);
-      tv.show();
+	  boolean zip = false;
+
+	  //remember to set the same utt as the one with which you recorded the trace
+	  UnitTypeTable utt = new UnitTypeTable();
+//	  UnitTypeTable utt = new UnitTypeTable(
+//			  UnitTypeTable.VERSION_ORIGINAL_FINETUNED,
+//			  UnitTypeTable.MOVE_CONFLICT_RESOLUTION_CANCEL_BOTH);	 
+	  
+	  Trace t;
+	  if(zip){
+		  ZipInputStream zipIs=new ZipInputStream(new FileInputStream(args[0]));
+		  zipIs.getNextEntry();
+		  t = new Trace(new SAXBuilder().build(zipIs).getRootElement(), utt);
+	  }else{ 
+		  t = new Trace(new SAXBuilder().build(args[0]).getRootElement(), utt);
+	  }
+	  
+	  JFrame tv = TraceVisualizer.newWindow("Demo", 800, 600, t, 1);
+	  tv.show();
   }    
 }
