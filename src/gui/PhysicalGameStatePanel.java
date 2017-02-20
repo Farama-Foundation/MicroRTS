473c726fdbbae10331b1ae64aae9a6a9c90f885b
Alberto Uriarte <albertouri@drexel.edu>
2017-02-19 20:45:55 -0500

    [GUI] Bigger health bars and added production bars

--- src/gui/PhysicalGameStatePanel.java
+++ src/gui/PhysicalGameStatePanel.java
@@ -30,32 +30,32 @@ import util.Pair;
 public class PhysicalGameStatePanel extends JPanel {
     public static int COLORSCHEME_BLACK = 1;
     public static int COLORSCHEME_WHITE = 2;
-    
+
     boolean fullObservability = true;
     int drawFromPerspectiveOfPlayer = -1;   // if fullObservability is false, and this is 0 or 1, it only draws what the specified player can see
     GameState gs = null;
-    
-    // Units to be highlighted (this is used, for example, by the MouseController, 
+
+    // Units to be highlighted (this is used, for example, by the MouseController,
     // to give feedback to the human, on which units are selectable.
     List<Unit> toHighLight = new LinkedList<Unit>();
     EvaluationFunction evalFunction = null;
-    
+
     // area to highlight: this can be used to highlight a rectangle of the game:
     int m_mouse_selection_x0 = -1;
     int m_mouse_selection_x1 = -1;
     int m_mouse_selection_y0 = -1;
-    int m_mouse_selection_y1 = -1;    
-    
+    int m_mouse_selection_y1 = -1;
+
     // the state observed by each player:
     PartiallyObservableGameState pogs[] = new PartiallyObservableGameState[2];
-    
+
     // Coordinates where things were drawn the last time this was redrawn:
     int last_start_x = 0;
     int last_start_y = 0;
     int last_grid = 0;
-    
+
     int colorScheme = COLORSCHEME_BLACK;
-    
+
     public PhysicalGameStatePanel(GameState a_gs) {
         this(a_gs, new SimpleEvaluationFunction());
     }
@@ -83,7 +83,7 @@ public class PhysicalGameStatePanel extends JPanel {
         if (colorScheme==COLORSCHEME_WHITE) setBackground(Color.WHITE);
     }
 
-    
+
     public PhysicalGameStatePanel(GameState a_gs, EvaluationFunction evalFunction, int cs) {
         gs = a_gs;
         if (gs!=null) {
@@ -96,7 +96,7 @@ public class PhysicalGameStatePanel extends JPanel {
         if (colorScheme==COLORSCHEME_BLACK) setBackground(Color.BLACK);
         if (colorScheme==COLORSCHEME_WHITE) setBackground(Color.WHITE);
     }
-    
+
     public static PhysicalGameStateJFrame newVisualizer(GameState a_gs) {
         return newVisualizer(a_gs, 320, 320, false, new SimpleEvaluationFunction(), COLORSCHEME_BLACK);
     }
@@ -104,15 +104,15 @@ public class PhysicalGameStatePanel extends JPanel {
     public static PhysicalGameStateJFrame newVisualizer(GameState a_gs, boolean a_showVisibility) {
         return newVisualizer(a_gs, 320, 320, a_showVisibility, new SimpleEvaluationFunction(), COLORSCHEME_BLACK);
     }
-        
+
     public static PhysicalGameStateJFrame newVisualizer(GameState a_gs, int dx, int dy) {
         return newVisualizer(a_gs, dx, dy, false, new SimpleEvaluationFunction(), COLORSCHEME_BLACK);
     }
-    
+
     public static PhysicalGameStateJFrame newVisualizer(GameState a_gs, int dx, int dy, boolean a_showVisibility) {
         return newVisualizer(a_gs, dx, dy, a_showVisibility, new SimpleEvaluationFunction(), COLORSCHEME_BLACK);
     }
-    
+
     public static PhysicalGameStateJFrame newVisualizer(GameState a_gs, int dx, int dy, boolean a_showVisibility, int cs) {
         return newVisualizer(a_gs, dx, dy, a_showVisibility, new SimpleEvaluationFunction(), cs);
     }
@@ -124,26 +124,26 @@ public class PhysicalGameStatePanel extends JPanel {
     public static PhysicalGameStateJFrame newVisualizer(GameState a_gs, int dx, int dy, boolean a_showVisibility, EvaluationFunction evalFunction, int cs) {
         PhysicalGameStatePanel ad = new PhysicalGameStatePanel(a_gs, evalFunction, cs);
         ad.fullObservability = !a_showVisibility;
-        
+
         PhysicalGameStateJFrame frame = null;
         frame = new PhysicalGameStateJFrame("Game State Visualizer", dx, dy, ad);
         return frame;
     }
-    
+
     public GameState getGameState() {
         return gs;
     }
-    
+
     public void setColorScheme(int cs) {
         colorScheme = cs;
         if (colorScheme==COLORSCHEME_BLACK) setBackground(Color.BLACK);
         if (colorScheme==COLORSCHEME_WHITE) setBackground(Color.WHITE);
     }
-    
+
     public int getColorScheme() {
         return colorScheme;
     }
-    
+
     public void setStateCloning(GameState a_gs) {
         gs = a_gs.clone();
         if (gs!=null) {
@@ -154,8 +154,8 @@ public class PhysicalGameStatePanel extends JPanel {
             pogs[1] = null;
         }
     }
-    
-    
+
+
     public void setStateDirect(GameState a_gs) {
         gs = a_gs;
         if (gs!=null) {
@@ -171,49 +171,49 @@ public class PhysicalGameStatePanel extends JPanel {
     public GameState getState() {
         return gs;
     }
-    
+
     public void clearHighlights() {
         toHighLight.clear();
     }
-    
-    
+
+
     public void highlight(Unit u) {
         toHighLight.add(u);
     }
-    
-    
+
+
     public Pair<Integer,Integer> getContentAtCoordinates(int x, int y) {
         // return the map coordiantes over which the coordinates are:
         // System.out.println(x + ", " + y + " -> last start: " + last_start_x + ", " + last_start_y);
         if (x<last_start_x) return null;
         if (y<last_start_y) return null;
-        
+
         int cellx = (x - last_start_x)/last_grid;
         int celly = (y - last_start_y)/last_grid;
-        
+
         if (cellx>=gs.getPhysicalGameState().getWidth()) return null;
         if (celly>=gs.getPhysicalGameState().getHeight()) return null;
-        
+
         return new Pair<Integer,Integer>(cellx,celly);
     }
-    
+
 
     public Pair<Integer,Integer> getContentAtCoordinatesBounded(int x, int y) {
         // return the map coordiantes over which the coordinates are:
         // System.out.println(x + ", " + y + " -> last start: " + last_start_x + ", " + last_start_y);
         if (x<last_start_x) x = last_start_x;
         if (y<last_start_y) y = last_start_y;
-        
+
         int cellx = (x - last_start_x)/last_grid;
         int celly = (y - last_start_y)/last_grid;
-        
+
         if (cellx>=gs.getPhysicalGameState().getWidth()) cellx = gs.getPhysicalGameState().getWidth()-1;
         if (celly>=gs.getPhysicalGameState().getHeight()) celly = gs.getPhysicalGameState().getHeight()-1;
-        
+
         return new Pair<Integer,Integer>(cellx,celly);
     }
-    
-    
+
+
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D)g;
@@ -226,13 +226,13 @@ public class PhysicalGameStatePanel extends JPanel {
             int y0 = Math.min(m_mouse_selection_y0, m_mouse_selection_y1);
             int y1 = Math.max(m_mouse_selection_y0, m_mouse_selection_y1);
             g.drawRect(x0, y0, x1 - x0, y1 - y0);
-        }        
+        }
     }
-    
-    
-    public static void draw(Graphics2D g2d, 
-                            PhysicalGameStatePanel panel, 
-                            int dx,int dy, 
+
+
+    public static void draw(Graphics2D g2d,
+                            PhysicalGameStatePanel panel,
+                            int dx,int dy,
                             GameState gs,
                             PartiallyObservableGameState pogs[],
                             int colorScheme,
@@ -247,20 +247,20 @@ public class PhysicalGameStatePanel extends JPanel {
         int grid = Math.min(gridx,gridy);
         int sizex = grid*pgs.getWidth();
         int sizey = grid*pgs.getHeight();
-                
+
         if (pogs!=null && pogs[0]!=null && pogs[1]!=null) {
             if (pogs[0].getTime() != gs.getTime()) {
-                // update 
+                // update
                 synchronized(gs) {
                     pogs[0] = new PartiallyObservableGameState(gs, 0);
                     pogs[1] = new PartiallyObservableGameState(gs, 1);
                 }
             }
         }
-        
+
         if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.WHITE);
         if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.BLACK);
-        
+
         int unitCount0 = 0;
         for (Unit unit : gs.getPhysicalGameState().getUnits()) {
             if (unit.getPlayer() == 0) {
@@ -279,7 +279,7 @@ public class PhysicalGameStatePanel extends JPanel {
 
         String info = "T: " + gs.getTime() + ", P₀: " + unitCount0 + " (" + eval0 + "), P₁: " + unitCount1 + " (" + eval1 + ")";
         g2d.drawString(info, 10, dy-15);
-        
+
 //        g.drawString(gs.getTime() + "", 10, getHeight()-15);
 
         AffineTransform t = g2d.getTransform();
@@ -294,7 +294,7 @@ public class PhysicalGameStatePanel extends JPanel {
             int last_start_y = dy/2 - sizey/2;
             g2d.translate(last_start_x, last_start_y);
         }
-        
+
         Color playerColor = null;
         Color wallColor = new Color(0, 0.33f, 0);
         Color po0color = new Color(0, 0, 0.25f);
@@ -314,7 +314,7 @@ public class PhysicalGameStatePanel extends JPanel {
                                 g2d.setColor(po1color);
                                 g2d.fillRect(j*grid, i*grid, grid, grid);
                             }
-                        }                        
+                        }
                     } else {
                         if (pogs[0].observable(j, i)) {
                             if (pogs[1].observable(j, i)) {
@@ -337,17 +337,17 @@ public class PhysicalGameStatePanel extends JPanel {
                     g2d.setColor(wallColor);
                     g2d.fillRect(j*grid, i*grid, grid, grid);
                 }
-            }            
+            }
         }
-        
+
         // draw grid:
         if (colorScheme==COLORSCHEME_BLACK) g2d.setColor(Color.GRAY);
         if (colorScheme==COLORSCHEME_WHITE) g2d.setColor(Color.BLACK);
-        for(int i = 0;i<=pgs.getWidth();i++) 
+        for(int i = 0;i<=pgs.getWidth();i++)
             g2d.drawLine(i*grid, 0, i*grid, pgs.getHeight()*grid);
-        for(int i = 0;i<=pgs.getHeight();i++) 
+        for(int i = 0;i<=pgs.getHeight();i++)
             g2d.drawLine(0, i*grid, pgs.getWidth()*grid, i*grid);
-        
+
         // draw the units:
         // this list copy is to prevent a concurrent modification exception
         List<Unit> l = new LinkedList<Unit>();
@@ -356,9 +356,9 @@ public class PhysicalGameStatePanel extends JPanel {
             int reduction = 0;
 
             if (!fullObservability &&
-                drawFromPerspectiveOfPlayer>=0 && 
+                drawFromPerspectiveOfPlayer>=0 &&
                 !pogs[drawFromPerspectiveOfPlayer].observable(u.getX(), u.getY())) continue;
-            
+
             // Draw the action:
             UnitActionAssignment uaa = gs.getActionAssignment(u);
             if (uaa!=null) {
@@ -384,7 +384,18 @@ public class PhysicalGameStatePanel extends JPanel {
                         break;
                     case UnitAction.TYPE_PRODUCE:
                         g2d.setColor(Color.BLUE);
-                        g2d.drawLine(u.getX()*grid+grid/2, u.getY()*grid+grid/2, u.getX()*grid+grid/2 + offsx, u.getY()*grid+grid/2 + offsy);
+                        g2d.drawLine(u.getX() * grid + grid / 2, u.getY() * grid + grid / 2, u.getX() * grid + grid / 2 + offsx, u.getY() * grid + grid / 2 + offsy);
+                        // draw building progress bar
+                        int ETA = uaa.time + uaa.action.ETA(uaa.unit) - gs.getTime();
+                        g2d.setColor(Color.BLUE);
+                        g2d.fillRect(u.getX() * grid + offsx, u.getY() * grid + offsy,
+                                grid - (int) (grid * (((float) ETA) / uaa.action.ETA(uaa.unit))), (int) (grid / 5.0));
+
+                        String txt = uaa.action.getUnitType().name;
+                        g2d.setColor(Color.BLUE);
+                        FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
+                        int width = fm.stringWidth(txt);
+                        g2d.drawString(txt, u.getX() * grid + grid / 2 - width / 2 + offsx, u.getY() * grid + grid / 2 + offsy);
                         break;
                     case UnitAction.TYPE_HARVEST:
                     case UnitAction.TYPE_RETURN:
@@ -432,20 +443,19 @@ public class PhysicalGameStatePanel extends JPanel {
                 g2d.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
                 g2d.setColor(playerColor);
                 if (panel!=null && panel.toHighLight.contains(u)) g2d.setColor(Color.green);
-                g2d.drawRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);        
+                g2d.drawRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
             } else {
                 g2d.fillOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
                 g2d.setColor(playerColor);
                 if (panel!=null && panel.toHighLight.contains(u)) g2d.setColor(Color.green);
-                g2d.drawOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);        
+                g2d.drawOval(u.getX()*grid+reduction, u.getY()*grid+reduction, grid-reduction*2, grid-reduction*2);
             }
 
             if (u.getType().isStockpile) {
                 // print the player resources in the base:
                 String txt = "" + pgs.getPlayer(u.getPlayer()).getResources();
                 g2d.setColor(Color.black);
-
-                FontMetrics fm = g2d.getFontMetrics( g2d.getFont() );
+                FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
                 int width = fm.stringWidth(txt);
                 g2d.drawString(txt, u.getX()*grid + grid/2 - width/2, u.getY()*grid + grid/2);
             }
@@ -454,24 +464,23 @@ public class PhysicalGameStatePanel extends JPanel {
             if (u.getResources()!=0) {
                 String txt = "" + u.getResources();
                 g2d.setColor(Color.black);
-
-                FontMetrics fm = g2d.getFontMetrics( g2d.getFont() );
+                FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
                 int width = fm.stringWidth(txt);
                 g2d.drawString(txt, u.getX()*grid + grid/2 - width/2, u.getY()*grid + grid/2);
             }
 
             if (u.getHitPoints()<u.getMaxHitPoints()) {
                 g2d.setColor(Color.RED);
-                g2d.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, grid, 2);
+                g2d.fillRect(u.getX() * grid, u.getY() * grid, grid, (int) (grid / 5.0));
                 g2d.setColor(Color.GREEN);
-                g2d.fillRect(u.getX()*grid+reduction, u.getY()*grid+reduction, (int)(grid*(((float)u.getHitPoints())/u.getMaxHitPoints())), 2);
+                g2d.fillRect(u.getX() * grid, u.getY() * grid, (int) (grid * (((float) u.getHitPoints()) / u.getMaxHitPoints())), (int) (grid / 5.0));
             }
-        }  
-        
+        }
+
         g2d.setTransform(t);
 
-    }    
-    
+    }
+
 
     public void resizeGameState(int width, int height) {
         if (width>=1 && height>=1) {
@@ -495,18 +504,18 @@ public class PhysicalGameStatePanel extends JPanel {
             pogs[1] = new PartiallyObservableGameState(gs, 1);
         }
     }
-    
-    
+
+
     public void setFullObservability(boolean fo) {
         fullObservability = fo;
     }
 
-    
+
     public void setDrawFromPerspectiveOfPlayer(int p) {
         drawFromPerspectiveOfPlayer = p;
     }
-    
-    
+
+
     public void gameStateUpdated() {
         if (gs!=null) {
             pogs[0] = new PartiallyObservableGameState(gs, 0);
