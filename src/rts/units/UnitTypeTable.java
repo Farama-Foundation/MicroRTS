799c161245302b84b72eae19ef94966edd6ec9c6
Alberto Uriarte <albertouri@drexel.edu>
2017-03-04 00:56:03 -0500

    New method producedBy

--- src/rts/units/UnitTypeTable.java
+++ src/rts/units/UnitTypeTable.java
@@ -16,31 +16,32 @@ public class UnitTypeTable implements Serializable {
     public static final int VERSION_ORIGINAL = 1;
     public static final int VERSION_ORIGINAL_FINETUNED = 2;
     public static final int VERSION_NON_DETERMINISTIC = 3;
-    
+
     public static final int MOVE_CONFLICT_RESOLUTION_CANCEL_BOTH = 1;   // (default)
     public static final int MOVE_CONFLICT_RESOLUTION_CANCEL_RANDOM = 2;   // (makes game non-deterministic)
-    public static final int MOVE_CONFLICT_RESOLUTION_CANCEL_ALTERNATING = 3;   
-    
+    public static final int MOVE_CONFLICT_RESOLUTION_CANCEL_ALTERNATING = 3;
+
     List<UnitType> unitTypes = new ArrayList<UnitType>();
     int moveConflictResolutionStrategy = MOVE_CONFLICT_RESOLUTION_CANCEL_BOTH;
-        
+    UnitType[] producedBy;
+
     public UnitTypeTable() {
         setUnitTypeTable(VERSION_ORIGINAL, MOVE_CONFLICT_RESOLUTION_CANCEL_BOTH);
     }
-    
+
     public UnitTypeTable(int version) {
         setUnitTypeTable(version, MOVE_CONFLICT_RESOLUTION_CANCEL_BOTH);
     }
-        
+
     public UnitTypeTable(int version, int crs) {
         setUnitTypeTable(version, crs);
     }
-    
-    
+
+
     public void setUnitTypeTable(int version, int crs) {
-        
+
         moveConflictResolutionStrategy = crs;
-        
+
         // Create the unit types:
         // RESOURCE:
         UnitType resource = new UnitType();
@@ -51,8 +52,8 @@ public class UnitTypeTable implements Serializable {
         resource.canMove = false;
         resource.canAttack = false;
         resource.sightRadius = 0;
-        addUnitType(resource);           
-                
+        addUnitType(resource);
+
         // BASE:
         UnitType base = new UnitType();
         base.name = "Base";
@@ -72,16 +73,16 @@ public class UnitTypeTable implements Serializable {
         base.sightRadius = 5;
         addUnitType(base);
 
-        // BARRACKS: 
+        // BARRACKS:
         UnitType barracks = new UnitType();
         barracks.name = "Barracks";
         barracks.cost = 5;
         barracks.hp = 4;
         switch(version) {
-            case VERSION_ORIGINAL: 
+            case VERSION_ORIGINAL:
                 barracks.produceTime = 200;
                 break;
-            case VERSION_ORIGINAL_FINETUNED: 
+            case VERSION_ORIGINAL_FINETUNED:
             case VERSION_NON_DETERMINISTIC:
                 barracks.produceTime = 100;
                 break;
@@ -93,8 +94,8 @@ public class UnitTypeTable implements Serializable {
         barracks.canAttack = false;
         barracks.sightRadius = 3;
         addUnitType(barracks);
-        
-        // WORKER: 
+
+        // WORKER:
         UnitType worker = new UnitType();
         worker.name = "Worker";
         worker.cost = 1;
@@ -121,9 +122,9 @@ public class UnitTypeTable implements Serializable {
         worker.canMove = true;
         worker.canAttack = true;
         worker.sightRadius = 3;
-        addUnitType(worker);   
-        
-        // LIGHT: 
+        addUnitType(worker);
+
+        // LIGHT:
         UnitType light = new UnitType();
         light.name = "Light";
         light.cost = 2;
@@ -148,9 +149,9 @@ public class UnitTypeTable implements Serializable {
         light.canMove = true;
         light.canAttack = true;
         light.sightRadius = 2;
-        addUnitType(light);           
+        addUnitType(light);
 
-        // HEAVY: 
+        // HEAVY:
         UnitType heavy = new UnitType();
         heavy.name = "Heavy";
         switch(version) {
@@ -166,12 +167,12 @@ public class UnitTypeTable implements Serializable {
         heavy.attackRange = 1;
         heavy.produceTime = 120;
         switch(version) {
-            case VERSION_ORIGINAL: 
+            case VERSION_ORIGINAL:
                 heavy.moveTime = 12;
                 heavy.hp = 4;
                 heavy.cost = 2;
                 break;
-            case VERSION_ORIGINAL_FINETUNED: 
+            case VERSION_ORIGINAL_FINETUNED:
             case VERSION_NON_DETERMINISTIC:
                 heavy.moveTime = 10;
                 heavy.hp = 8;
@@ -185,9 +186,9 @@ public class UnitTypeTable implements Serializable {
         heavy.canMove = true;
         heavy.canAttack = true;
         heavy.sightRadius = 2;
-        addUnitType(heavy);           
+        addUnitType(heavy);
 
-        // RANGED: 
+        // RANGED:
         UnitType ranged = new UnitType();
         ranged.name = "Ranged";
         ranged.cost = 2;
@@ -212,26 +213,35 @@ public class UnitTypeTable implements Serializable {
         ranged.canMove = true;
         ranged.canAttack = true;
         ranged.sightRadius = 3;
-        addUnitType(ranged);     
-        
+        addUnitType(ranged);
 
-        base.produces.add(worker);  
-        barracks.produces.add(light);
-        barracks.produces.add(heavy); 
-        barracks.produces.add(ranged);
-        worker.produces.add(base);
-        worker.produces.add(barracks);
+        producedBy = new UnitType[unitTypes.size()];
+        produces(base, worker);
+        produces(barracks, light);
+        produces(barracks, heavy);
+        produces(barracks, ranged);
+        produces(worker, base);
+        produces(worker, barracks);
     }
-    
+
     public void addUnitType(UnitType ut) {
         ut.ID = unitTypes.size();
         unitTypes.add(ut);
     }
-    
+
+    public void produces(UnitType producer, UnitType produces) {
+        producer.produces.add(produces);
+        producedBy[produces.ID] = producer;
+    }
+
+    public UnitType producedBy(int ID) {
+        return producedBy[ID];
+    }
+
     public UnitType getUnitType(int ID) {
         return unitTypes.get(ID);
     }
-    
+
     public UnitType getUnitType(String name) {
         for(UnitType ut:unitTypes) {
             if (ut.name.equals(name)) return ut;
@@ -242,10 +252,10 @@ public class UnitTypeTable implements Serializable {
     public List<UnitType> getUnitTypes() {
         return unitTypes;
     }
-    
+
     public int getMoveConflictResolutionStrategy() {
         return moveConflictResolutionStrategy;
     }
-    
-    
+
+
 }
