4e91351d0840fb5a8498ce0a49b6163d6525cbf1
Santiago Ontañón <santi@n3-115-115.dhcp.drexel.edu>
2017-06-04 11:58:18 -0400

    added examples of how to run an AI using a socket

---
+++ src/tests/sockets/RunClientExample.java
@@ -0,0 +1,73 @@
+ /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests.sockets;
+
+import ai.core.AI;
+import ai.*;
+import ai.socket.SocketAI;
+import gui.PhysicalGameStatePanel;
+import javax.swing.JFrame;
+import rts.GameState;
+import rts.PhysicalGameState;
+import rts.PlayerAction;
+import rts.units.UnitTypeTable;
+
+/**
+ *
+ * @author santi
+ * 
+ * Once you have the server running (for example, run "RunServerExample.java"),
+ * set the proper IP and port in the variable below, and run this file.
+ * One of the AIs (ai1) is run remotely using the server.
+ * 
+ * Notice that as many AIs as needed can connect to the same server. For
+ * example, uncomment line 44 below and comment 45, to see two AIs using the same server.
+ * 
+ */
+public class RunClientExample {
+    public static void main(String args[]) throws Exception {
+        String serverIP = "127.0.0.1";
+        int serverPort = 9898;
+        
+        
+        UnitTypeTable utt = new UnitTypeTable();
+        PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/basesWorkers16x16.xml", utt);
+
+        GameState gs = new GameState(pgs, utt);
+        int MAXCYCLES = 5000;
+        int PERIOD = 20;
+        boolean gameover = false;
+        
+        AI ai1 = new SocketAI(100,0, serverIP, serverPort, SocketAI.LANGUAGE_XML, utt);
+//        AI ai2 = new SocketAI(100,0, serverIP, serverPort, SocketAI.LANGUAGE_XML, utt);
+        AI ai2 = new RandomBiasedAI();
+        
+        ai1.reset();
+        ai2.reset();
+
+        JFrame w = PhysicalGameStatePanel.newVisualizer(gs,640,640,false,PhysicalGameStatePanel.COLORSCHEME_BLACK);
+
+        long nextTimeToUpdate = System.currentTimeMillis() + PERIOD;
+        do{
+            if (System.currentTimeMillis()>=nextTimeToUpdate) {
+                PlayerAction pa1 = ai1.getAction(0, gs);
+                PlayerAction pa2 = ai2.getAction(1, gs);
+                gs.issueSafe(pa1);
+                gs.issueSafe(pa2);
+
+                // simulate:
+                gameover = gs.cycle();
+                w.repaint();
+                nextTimeToUpdate+=PERIOD;
+            } else {
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }while(!gameover && gs.getTime()<MAXCYCLES);        
+    }    
+}
---
+++ src/tests/sockets/RunServerExample.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests.sockets;
+
+import ai.abstraction.WorkerRush;
+import ai.core.AIWithComputationBudget;
+import ai.socket.XMLSocketWrapperAI;
+import rts.units.UnitTypeTable;
+
+/**
+ *
+ * @author santi
+ * 
+ * Run this file first in the computer where you want the server to
+ * be running.
+ * 
+ */
+public class RunServerExample {
+    public static void main(String args[]) throws Exception {
+        AIWithComputationBudget ai = new WorkerRush(new UnitTypeTable());
+        int port = 9898;
+        
+        XMLSocketWrapperAI.runServer(ai, port);
+    }
+}
