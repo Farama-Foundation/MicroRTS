9d6d0d07a061bd7ef6aa70611533dc60e3c6fa5e
anderson <andersonrochatavares@gmail.com>
2017-11-27 09:30:08 -0200

    created javadoc

--- src/rts/Trace.java
+++ src/rts/Trace.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rts;
 
 import java.util.LinkedList;
@@ -13,17 +9,26 @@ import util.Pair;
 import util.XMLWriter;
 
 /**
- *
+ * Contains actions executed throughout a match, serving as a 'replay' class
  * @author santi
  */
 public class Trace {
     UnitTypeTable utt = null;
     List<TraceEntry> entries = new LinkedList<TraceEntry>();
     
+    /**
+     * Constructs from a UnitTypeTable
+     * @param a_utt
+     */
     public Trace(UnitTypeTable a_utt) {
         utt = a_utt;
     }
 
+    /**
+     * Returns the list of entries, where each entry corresponds to 
+     * actions executed in a frame
+     * @return
+     */
     public List<TraceEntry> getEntries() {
         return entries;
     }
@@ -32,19 +37,39 @@ public class Trace {
         return utt;
     }
     
+    
+    /**
+     * Returns the number of the last frame stored in this trace
+     * @return
+     */
     public int getLength() {
         return entries.get(entries.size()-1).getTime();
     }
     
+    /**
+     * Returns the index of the winner player.
+     * Or returns -1 if there are no entries or the game is not over
+     * @return
+     */
     public int winner() {
-        if (entries.isEmpty()) return -1;
-        return entries.get(entries.size()-1).pgs.winner();
+		if (entries.isEmpty())
+			return -1;
+		
+		return entries.get(entries.size() - 1).pgs.winner();
     }    
     
+    /**
+     * Adds a new entry, which corresponds to a frame
+     * @param te
+     */
     public void addEntry(TraceEntry te) {
         entries.add(te);
     }
     
+    /**
+     * Writes a XML representation
+     * @param w
+     */
     public void toxml(XMLWriter w) {
        w.tag(this.getClass().getName());
        utt.toxml(w);
@@ -54,19 +79,27 @@ public class Trace {
        w.tag("/" + this.getClass().getName());
     }
         
-    
+    /**
+     * Constructs the Trace from a XML element
+     * @param e
+     */
     public Trace(Element e) {
         utt = UnitTypeTable.fromXML(e.getChild(UnitTypeTable.class.getName()));
         Element entries_e = e.getChild("entries");
         
-        for(Object o:entries_e.getChildren()) {
-            Element entry_e = (Element)o;
-            entries.add(new TraceEntry(entry_e, utt));
-        }
+		for (Object o : entries_e.getChildren()) {
+			Element entry_e = (Element) o;
+			entries.add(new TraceEntry(entry_e, utt));
+		}
     }    
     
 
-    // this loads a trace ignoring the UTT specified in the trace:
+    /**
+     * Constructs the Trace from a XML element, overriding the UnitTypeTable of that 
+     * element with one provided 
+     * @param e
+     * @param a_utt
+     */
     public Trace(Element e, UnitTypeTable a_utt) {
         utt = a_utt;
         Element entries_e = e.getChild("entries");
@@ -78,66 +111,75 @@ public class Trace {
     }    
 
     
-    // Note: this function is slow, since it has to simulate the game from the very beginning
-    //       in order to get the appropriate unit actions. So, do not use in the internal loop
-    //       of any AI!
-    GameState getGameStateAtCycle_cache = null; // this accelerates the function below if traversing a trace sequentially
+    
+    /**
+     * this accelerates the function below if traversing a trace sequentially
+     */
+    GameState getGameStateAtCycle_cache = null; 
+    
+    /**
+     * Simulates the game from the from the last cached cycle (initialized as null) 
+     * to get the appropriate unit actions.
+     * Thus, this function can be slow, do not use in the internal loop of any AI!
+     * @param cycle
+     * @return
+     */
     public GameState getGameStateAtCycle(int cycle) {
         GameState gs = null;
-        for(TraceEntry te:getEntries()) {
-            if (gs==null) {
-                if (getGameStateAtCycle_cache != null && cycle >= getGameStateAtCycle_cache.getTime()) {
-                    if (te.getTime() < getGameStateAtCycle_cache.getTime()) {
-                        continue;
-                    } else {
-                        gs = getGameStateAtCycle_cache.clone();
-                    }
-                } else {
-                    gs = new GameState(te.getPhysicalGameState().clone(), utt);            
-                }
-            }
-            
-            while(gs.getTime()<te.getTime() && gs.getTime()<cycle) {
-                gs.cycle();
-            }
+		for (TraceEntry te : getEntries()) {
+			if (gs == null) {
+				if (getGameStateAtCycle_cache != null && cycle >= getGameStateAtCycle_cache.getTime()) {
+					if (te.getTime() < getGameStateAtCycle_cache.getTime()) {
+						continue;
+					} else {
+						gs = getGameStateAtCycle_cache.clone();
+					}
+				} else {
+					gs = new GameState(te.getPhysicalGameState().clone(), utt);
+				}
+			}
 
-            
-            // synchronize the traces (some times the unit IDs might go off):
-            for(Unit u1:gs.getUnits()) {
-                for(Unit u2:te.getPhysicalGameState().getUnits()) {
-                    if (u1.getX()==u2.getX() &&
-                        u1.getY()==u2.getY() &&
-                        u1.getType() == u2.getType() &&
-                        u1.getID() != u2.getID()) {
-                        u1.setID(u2.getID());
-                    }
-                }
-            }
+			while (gs.getTime() < te.getTime() && gs.getTime() < cycle) {
+				gs.cycle();
+			}
 
-            if (gs.getTime()==cycle) {
-                getGameStateAtCycle_cache = gs;
-                return gs;
-            }
-            
-            PlayerAction pa0 = new PlayerAction();
-            PlayerAction pa1 = new PlayerAction();
-            for(Pair<Unit,UnitAction> tmp:te.getActions()) {
-                if (tmp.m_a.getPlayer()==0) pa0.addUnitAction(tmp.m_a, tmp.m_b);
-                if (tmp.m_a.getPlayer()==1) pa1.addUnitAction(tmp.m_a, tmp.m_b);
-            }
-            gs.issueSafe(pa0);
-            gs.issueSafe(pa1);
+			// synchronize the traces (some times the unit IDs might go off):
+			for (Unit u1 : gs.getUnits()) {
+				for (Unit u2 : te.getPhysicalGameState().getUnits()) {
+					if (u1.getX() == u2.getX() && u1.getY() == u2.getY() && u1.getType() == u2.getType()
+							&& u1.getID() != u2.getID()) {
+						u1.setID(u2.getID());
+					}
+				}
+			}
 
-            if (gs.getTime()==cycle) {
-                getGameStateAtCycle_cache = gs;
-                return gs;
-            }
+			if (gs.getTime() == cycle) {
+				getGameStateAtCycle_cache = gs;
+				return gs;
+			}
 
-        }
-        while(gs.getTime()<cycle) gs.cycle();
-        
-        getGameStateAtCycle_cache = gs;
-        return gs;
+			PlayerAction pa0 = new PlayerAction();
+			PlayerAction pa1 = new PlayerAction();
+			for (Pair<Unit, UnitAction> tmp : te.getActions()) {
+				if (tmp.m_a.getPlayer() == 0)
+					pa0.addUnitAction(tmp.m_a, tmp.m_b);
+				if (tmp.m_a.getPlayer() == 1)
+					pa1.addUnitAction(tmp.m_a, tmp.m_b);
+			}
+			gs.issueSafe(pa0);
+			gs.issueSafe(pa1);
+
+			if (gs.getTime() == cycle) {
+				getGameStateAtCycle_cache = gs;
+				return gs;
+			}
+
+		}
+		while (gs.getTime() < cycle)
+			gs.cycle();
+
+		getGameStateAtCycle_cache = gs;
+		return gs;
     }    
     
 }
