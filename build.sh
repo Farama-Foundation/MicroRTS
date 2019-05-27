5ecf12475c72db657d4ad9fadc10d894fbf5eaca
Costa Huang <costa.huang@outlook.com>
2019-05-27 18:52:35 -0400

    Master # Update the build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 mkdir build
 javac -d "./build" -cp "./lib/*" -sourcepath "./src" ./src/tests/*.java ./src/tests/sockets/*.java
+cp -a lib/. build/
 cd build
+for i in *.jar; do
+    echo "adding dependency $i"
+    jar xf $i
+done
 jar cvf microrts.jar *
 mv microrts.jar ../microrts.jar
 cd ..
