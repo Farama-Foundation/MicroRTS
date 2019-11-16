99f440929fcb8d6451530365ddb57a30c77bafcb
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-11-16 02:41:05 -0300

    removed unused variables

--- src/rts/Trace.java
+++ src/rts/Trace.java
@@ -119,17 +119,11 @@ public class Trace {
     }
     
     public void toZip(String path) {
-    	String zipPath;
-    	//zipPath must end with .zip and path with .xml
     	if(path.endsWith(".zip")) {
-    		zipPath = path;
-    		path.replaceFirst("[.][^.]+$", ".xml"); // replaces .zip by .xml
+            path.replaceFirst("[.][^.]+$", ".xml"); // replaces .zip by .xml
     	}
-    	else {
-    		zipPath = path + ".zip";    		
-    	}
-    	
-    	File f = new File(path);
+
+        File f = new File(path);
     	ZipOutputStream out;
 		try {
 			out = new ZipOutputStream(new FileOutputStream(f));
