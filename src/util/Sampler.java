f10512e0457461ffcb928a49a6bbb0fe887bd6ae
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-11-19 15:18:05 -0300

    found another instance of unnecessary boxing

--- src/util/Sampler.java
+++ src/util/Sampler.java
@@ -98,11 +98,11 @@ public class Sampler {
         for (int i = 0; i < distribution.length; i++) {
             double f = distribution[i];
             if (f == max) {
-                best.add(new Integer(i));
+                best.add(i);
             } else {
                 if (f > max) {
                     best.clear();
-                    best.add(new Integer(i));
+                    best.add(i);
                     max = f;
                 }
             }
