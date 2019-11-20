29e11a8e4ac243d10d0d4fb8f760104d70c51144
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-11-20 03:51:04 -0300

    ternary operator can be just the result of its boolean expression

--- src/ai/ahtn/planner/AdversarialBoundedDepthPlannerAlphaBeta.java
+++ src/ai/ahtn/planner/AdversarialBoundedDepthPlannerAlphaBeta.java
@@ -361,7 +361,7 @@ public class AdversarialBoundedDepthPlannerAlphaBeta {
                 int nPlayoutsleft = maxPlayouts - nPlayouts;
                 if (maxPlayouts<0 || nPlayoutsleft>nPlayoutsUSedLastTime) {
                     if (DEBUG>=1) System.out.println("last time we used " + nPlayoutsUSedLastTime + ", and there are " + nPlayoutsleft + " left, trying one more depth!");
-                    best = planner.getBestPlan(timeLimit, maxPlayouts, (bestLastDepth==null ? true:false));                
+                    best = planner.getBestPlan(timeLimit, maxPlayouts, (bestLastDepth == null));
                 } else {
                     if (DEBUG>=1) System.out.println("last time we used " + nPlayoutsUSedLastTime + ", and there are only " + nPlayoutsleft + " left..., canceling search");
                 }
