f544c8ec0ad2167a58fc7564927ca55f11c8fbef
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-11-24 21:53:39 -0300

    RemoteGame also uses Game object, reducing duplicate code

--- src/rts/RemoteGame.java
+++ src/rts/RemoteGame.java
@@ -2,35 +2,17 @@ package rts;
 
 import ai.core.AI;
 import ai.socket.SocketAI;
-import gui.PhysicalGameStatePanel;
 import java.net.Socket;
-import javax.swing.JFrame;
 import rts.units.UnitTypeTable;
 
-class RemoteGame extends Thread {
+class RemoteGame implements Runnable {
 
     private Socket socket;
-    private UnitTypeTable unitTypeTable;
-    private PhysicalGameState pgs;
-    private GameState gameState;
     private GameSettings gameSettings;
-    private boolean gameOver = false;
-
-    private static int PERIOD = 20;
-
 
     RemoteGame(Socket socket, GameSettings gameSettings) {
         this.socket = socket;
         this.gameSettings = gameSettings;
-
-        this.unitTypeTable = new UnitTypeTable(gameSettings.getUTTVersion(),gameSettings.getConflictPolicy());
-
-        try {
-            this.pgs = PhysicalGameState.load(gameSettings.getMapLocation(), unitTypeTable);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        this.gameState = new GameState(pgs, unitTypeTable);
     }
 
     /**
@@ -39,6 +21,9 @@ class RemoteGame extends Thread {
     @Override
     public void run() {
         try {
+            rts.units.UnitTypeTable unitTypeTable = new rts.units.UnitTypeTable(
+                gameSettings.getUTTVersion(), gameSettings.getConflictPolicy());
+
             // Generate players
             // player 1 is created from SocketAI
             AI player_one = SocketAI.createFromExistingSocket(100, 0, unitTypeTable,
@@ -49,50 +34,10 @@ class RemoteGame extends Thread {
                 .getConstructor(UnitTypeTable.class);
             AI player_two = (AI) cons2.newInstance(unitTypeTable);
 
-            // Reset all players
-            player_one.reset();
-            player_two.reset();
-
-            // allow for pre-game analysis
-            player_one.preGameAnalysis(gameState,0);
-            player_two.preGameAnalysis(gameState,0);
-
-            // Setup UI
-            JFrame w = PhysicalGameStatePanel.newVisualizer(gameState,640, 640, false, PhysicalGameStatePanel.COLORSCHEME_BLACK);
-
-            long nextTimeToUpdate = System.currentTimeMillis() + PERIOD;
-            do {
-                if (System.currentTimeMillis() >= nextTimeToUpdate) {
-
-                    GameState playerOneGameState = gameSettings.isPartiallyObservable() ? new PartiallyObservableGameState(gameState,0) : gameState;
-                    GameState playerTwoGameState = gameSettings.isPartiallyObservable() ? new PartiallyObservableGameState(gameState,1) : gameState;
-
-                    PlayerAction pa1 = player_one.getAction(0, playerOneGameState);
-                    PlayerAction pa2 = player_two.getAction(1, playerTwoGameState);
-
-                    gameState.issueSafe(pa1);
-                    gameState.issueSafe(pa2);
-
-                    // simulate:
-                    gameOver = gameState.cycle();
-
-                    w.repaint();
-                    nextTimeToUpdate += PERIOD;
-                } else {
-                    try {
-                        Thread.sleep(1);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            } while (!gameOver && gameState.getTime() < gameSettings.getMaxCycles());
-            player_one.gameOver(gameState.winner());
-            player_two.gameOver(gameState.winner());
-        } catch ( Exception e ) {
+            Game game = new Game(gameSettings, player_one, player_two);
+            game.start();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
