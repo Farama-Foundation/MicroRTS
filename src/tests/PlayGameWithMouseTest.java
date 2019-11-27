8f0be3c25f8a9f754b21fd80d60b4bb94a4d6375
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-11-26 21:03:37 -0300

    changed mouse tests to use MouseGame class, with success

--- src/tests/PlayGameWithMouseTest.java
+++ src/tests/PlayGameWithMouseTest.java
@@ -14,6 +14,7 @@ import gui.PhysicalGameStateMouseJFrame;
 import gui.PhysicalGameStatePanel;
 import rts.Game;
 import rts.GameState;
+import rts.MouseGame;
 import rts.PhysicalGameState;
 import rts.PlayerAction;
 import rts.units.UnitTypeTable;
@@ -24,19 +25,12 @@ import rts.units.UnitTypeTable;
  */
 public class PlayGameWithMouseTest {
     public static void main(String args[]) throws Exception {
-        UnitTypeTable utt = new UnitTypeTable();
-        PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/basesWorkers16x16.xml", utt);
-
-        GameState gs = new GameState(pgs, utt);
         int PERIOD = 100;
-        PhysicalGameStatePanel pgsp = new PhysicalGameStatePanel(gs);
-        PhysicalGameStateMouseJFrame w = new PhysicalGameStateMouseJFrame("Game State Visualizer (Mouse)",640,640, pgsp);
 
-        AI ai1 = new MouseController(w);
-        AI ai2 = new ContinuingAI(new NaiveMCTS(PERIOD, -1, 100, 20, 0.33f, 0.0f, 0.75f, new RandomBiasedAI(), new SimpleEvaluationFunction(), true));
+        AI opponent = new ContinuingAI(new NaiveMCTS(PERIOD, -1, 100, 20, 0.33f, 0.0f, 0.75f, new RandomBiasedAI(), new SimpleEvaluationFunction(), true));
 
-        Game game = new Game( utt, "maps/16x16/basesWorkers16x16.xml", false, false, 5000, PERIOD, ai1, ai2);
-        game.start(w);
+        Game game = new MouseGame( new UnitTypeTable(), "maps/16x16/basesWorkers16x16.xml", false, false, 5000, PERIOD, opponent);
+        game.start();
         
         System.out.println("Game Over");
     }    
