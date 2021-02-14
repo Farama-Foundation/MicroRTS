c042b09a4e35adeba2adfca35cf102f08bfa22b6
Costa Huang <costa.huang@outlook.com>
2021-02-14 13:16:48 -0500

    support self play

--- src/tests/JNIGridnetClientSelfPlay.java
+++ src/tests/JNIGridnetClientSelfPlay.java
@@ -8,6 +8,9 @@ import java.io.Writer;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 import java.awt.image.BufferedImage;
 import java.io.StringWriter;
 import java.awt.image.DataBufferByte;
@@ -34,6 +37,8 @@ import rts.UnitActionAssignment;
 import rts.units.Unit;
 import rts.units.UnitTypeTable;
 import weka.core.pmml.jaxbbindings.False;
+import tests.JNIGridnetVecClient;
+import tests.JNIGridnetClient.Response;
 
 /**
  *
@@ -49,11 +54,12 @@ import weka.core.pmml.jaxbbindings.False;
  *         the same server.
  * 
  */
-public class JNIGridnetClient {
+public class JNIGridnetClientSelfPlay {
 
     PhysicalGameStateJFrame w;
-    public JNIInterface ai1;
-    public AI ai2;
+    public JNIInterface[] ais = new JNIInterface[2];
+    // public JNIInterface ai1;
+    // public JNIInterface ai2;
     PhysicalGameState pgs;
     GameState gs;
     UnitTypeTable utt;
@@ -64,47 +70,21 @@ public class JNIGridnetClient {
     boolean layerJSON = true;
     public int renderTheme = PhysicalGameStatePanel.COLORSCHEME_WHITE;
     public int maxAttackRadius;
+    public int numPlayers = 2;
 
     // storage
-    int[][][] masks;
-    double[] rewards;
-    boolean[] dones;
-    Response response;
-    PlayerAction pa1;
-    PlayerAction pa2;
+    int[][][][] masks = new int[2][][][];
+    double[][] rewards = new double[2][];
+    boolean[][] dones = new boolean[2][];
+    Response[] response = new Response[2];
+    PlayerAction[] pas = new PlayerAction[2];
 
-    public static class Response {
-        public int[][][] observation;
-        public double[] reward;
-        public boolean[] done;
-        public String info;
-
-        public Response(int[][][] observation, double reward[], boolean done[], String info) {
-            this.observation = observation;
-            this.reward = reward;
-            this.done = done;
-            this.info = info;
-        }
-
-        public void set(int[][][] observation, double reward[], boolean done[], String info) {
-            this.observation = observation;
-            this.reward = reward;
-            this.done = done;
-            this.info = info;
-        }
-    }
-
-    public JNIGridnetClient(RewardFunctionInterface[] a_rfs, String a_micrortsPath, String a_mapPath, AI a_ai2, UnitTypeTable a_utt) throws Exception{
+    public JNIGridnetClientSelfPlay(RewardFunctionInterface[] a_rfs, String a_micrortsPath, String a_mapPath, UnitTypeTable a_utt) throws Exception{
         micrortsPath = a_micrortsPath;
         mapPath = a_mapPath;
         rfs = a_rfs;
         utt = a_utt;
         maxAttackRadius = utt.getMaxAttackRange() * 2 + 1;
-        ai1 = new JNIAI(100, 0, utt);
-        ai2 = a_ai2;
-        if (ai2 == null) {
-            throw new Exception("no ai2 was chosen");
-        }
         if (micrortsPath.length() != 0) {
             this.mapPath = Paths.get(micrortsPath, mapPath).toString();
         }
@@ -113,10 +93,13 @@ public class JNIGridnetClient {
         pgs = PhysicalGameState.load(mapPath, utt);
 
         // initialize storage
-        masks = new int[pgs.getHeight()][pgs.getWidth()][1+6+4+4+4+4+utt.getUnitTypes().size()+maxAttackRadius*maxAttackRadius];
-        rewards = new double[rfs.length];
-        dones = new boolean[rfs.length];
-        response = new Response(null, null, null, null);
+        for (int i = 0; i < numPlayers; i++) {
+            ais[i] = new JNIAI(100, 0, utt);
+            masks[i] = new int[pgs.getHeight()][pgs.getWidth()][1+6+4+4+4+4+utt.getUnitTypes().size()+maxAttackRadius*maxAttackRadius];
+            rewards[i] = new double[rfs.length];
+            dones[i] = new boolean[rfs.length];
+            response[i] = new Response(null, null, null, null);
+        }
     }
 
     public byte[] render(boolean returnPixels) throws Exception {
@@ -138,40 +121,39 @@ public class JNIGridnetClient {
         return data.getData();
     }
 
-    public Response gameStep(int[][] action, int player) throws Exception {
-        pa1 = ai1.getAction(player, gs, action);
-        pa2 = ai2.getAction(1 - player, gs);
-
-        gs.issueSafe(pa1);
-        gs.issueSafe(pa2);
+    public void gameStep(int[][] action1, int[][] action2) throws Exception {
         TraceEntry te  = new TraceEntry(gs.getPhysicalGameState().clone(), gs.getTime());
-        te.addPlayerAction(pa1.clone());
-        te.addPlayerAction(pa2.clone());
-
+        for (int i = 0; i < numPlayers; i++) {
+            pas[i] = i == 0 ? ais[i].getAction(i, gs, action1) : ais[i].getAction(i, gs, action2);
+            gs.issueSafe(pas[i]);
+            te.addPlayerAction(pas[i].clone());
+        }
         // simulate:
         gameover = gs.cycle();
         if (gameover) {
             // ai1.gameOver(gs.winner());
-            ai2.gameOver(gs.winner());
+            // ai2.gameOver(gs.winner());
         }
-        for (int i = 0; i < rewards.length; i++) {
-            rfs[i].computeReward(player, 1 - player, te, gs);
-            dones[i] = rfs[i].isDone();
-            rewards[i] = rfs[i].getReward();
+
+        for (int i = 0; i < numPlayers; i++) {
+            for (int j = 0; j < rfs.length; j++) {
+                rfs[j].computeReward(i, 1 - i, te, gs);
+                rewards[i][j] = rfs[j].getReward();
+                dones[i][j] = rfs[j].isDone();
+            }
+            response[i].set(
+                ais[i].getObservation(i, gs),
+                rewards[i],
+                dones[i],
+                "{}");
         }
-        response.set(
-            ai1.getObservation(player, gs),
-            rewards,
-            dones,
-            ai1.computeInfo(player, gs));
-        return response;
     }
 
     public int[][][] getMasks(int player) throws Exception {
-        for (int i = 0; i < masks.length; i++) {
-            for (int j = 0; j < masks[0].length; j++) {
-                for (int k = 0; k < masks[0][0].length; k++) {
-                    masks[i][j][k] = 0;
+        for (int i = 0; i < masks[0].length; i++) {
+            for (int j = 0; j < masks[0][0].length; j++) {
+                for (int k = 0; k < masks[0][0][0].length; k++) {
+                    masks[player][i][j][k] = 0;
                 }
             }
         }
@@ -179,11 +161,11 @@ public class JNIGridnetClient {
             Unit u = pgs.getUnits().get(i);
             UnitActionAssignment uaa = gs.getUnitActions().get(u);
             if (u.getPlayer() == player && uaa == null) {
-                masks[u.getY()][u.getX()][0] = 1;
-                UnitAction.getValidActionArray(u, gs, utt, masks[u.getY()][u.getX()], maxAttackRadius);
+                masks[player][u.getY()][u.getX()][0] = 1;
+                UnitAction.getValidActionArray(u, gs, utt, masks[player][u.getY()][u.getX()], maxAttackRadius);
             }
         }
-        return masks;
+        return masks[player];
     }
 
     public String sendUTT() throws Exception {
@@ -192,23 +174,27 @@ public class JNIGridnetClient {
         return w.toString(); // now it works fine
     }
 
-    public Response reset(int player) throws Exception {
-        ai1.reset();
-        ai2 = ai2.clone();
-        ai2.reset();
+    public void reset() throws Exception {
         pgs = PhysicalGameState.load(mapPath, utt);
         gs = new GameState(pgs, utt);
-
-        for (int i = 0; i < rewards.length; i++) {
-            rewards[i] = 0;
-            dones[i] = false;
+        for (int i = 0; i < numPlayers; i++) {
+            ais[i].reset();
+            for (int j = 0; j < rewards.length; j++) {
+                rewards[i][j] = 0;
+                dones[i][j] = false;
+            }
+            response[i].set(
+                ais[i].getObservation(i, gs),
+                rewards[i],
+                dones[i],
+                "{}");
         }
-        response.set(
-            ai1.getObservation(player, gs),
-            rewards,
-            dones,
-            "{}");
-        return response;
+
+        // return response;
+    }
+
+    public Response getResponse(int player) {
+        return response[player];
     }
 
     public void close() throws Exception {
--- src/tests/JNIGridnetVecClient.java
+++ src/tests/JNIGridnetVecClient.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.awt.image.BufferedImage;
@@ -39,6 +40,7 @@ import rts.units.Unit;
 import rts.units.UnitTypeTable;
 import tests.JNIGridnetClient;
 import tests.JNIGridnetClient.Response;
+import tests.JNIGridnetClientSelfPlay;
 
 /**
  *
@@ -56,9 +58,9 @@ import tests.JNIGridnetClient.Response;
  */
 public class JNIGridnetVecClient {
     public JNIGridnetClient[] clients;
+    public JNIGridnetClientSelfPlay[] selfPlayClients;
     public int maxSteps;
     public int[] envSteps; 
-    public ExecutorService pool;
     public RewardFunctionInterface[] rfs;
     public UnitTypeTable utt;
 
@@ -70,26 +72,40 @@ public class JNIGridnetVecClient {
     JNIGridnetClient.Response[] rs;
     Responses responses;
 
-    public JNIGridnetVecClient(int a_num_envs, int a_max_steps, RewardFunctionInterface[] a_rfs, String a_micrortsPath, String a_mapPath,
+    double[] terminalReward1;
+    boolean[] terminalRone1;
+    double[] terminalReward2;
+    boolean[] terminalRone2;
+
+    public JNIGridnetVecClient(int a_num_selfplayenvs, int a_num_envs, int a_max_steps, RewardFunctionInterface[] a_rfs, String a_micrortsPath, String a_mapPath,
             AI[] a_ai2s, UnitTypeTable a_utt) throws Exception {
         maxSteps = a_max_steps;
         utt = a_utt;
+        rfs = a_rfs;
+
+        // initialize clients
+        envSteps = new int[a_num_selfplayenvs + a_num_envs];
+        selfPlayClients = new JNIGridnetClientSelfPlay[a_num_selfplayenvs/2];
+        for (int i = 0; i < selfPlayClients.length; i++) {
+            selfPlayClients[i] = new JNIGridnetClientSelfPlay(a_rfs, a_micrortsPath, a_mapPath, a_utt);
+        }
         clients = new JNIGridnetClient[a_num_envs];
-        for (int i = 0; i < a_num_envs; i++) {
+        for (int i = 0; i < clients.length; i++) {
             clients[i] = new JNIGridnetClient(a_rfs, a_micrortsPath, a_mapPath, a_ai2s[i], a_utt);
         }
-        rfs = a_rfs;
-        envSteps = new int[a_num_envs];
-        pool = Executors.newFixedThreadPool(64);
-        
+
         // initialize storage
-        JNIGridnetClient.Response r = clients[0].reset(0);
-        int s1 = a_num_envs, s2 = r.observation.length, s3 = r.observation[0].length,
+        JNIGridnetClient.Response r = new JNIGridnetClient(a_rfs, a_micrortsPath, a_mapPath, new PassiveAI(a_utt), a_utt).reset(0);
+        int s1 = a_num_selfplayenvs + a_num_envs, s2 = r.observation.length, s3 = r.observation[0].length,
                 s4 = r.observation[0][0].length;
         masks = new int[s1][][][];
         observation = new int[s1][s2][s3][s4];
         reward = new double[s1][rfs.length];
         done = new boolean[s1][rfs.length];
+        terminalReward1 = new double[rfs.length];
+        terminalRone1 = new boolean[rfs.length];
+        terminalReward2 = new double[rfs.length];
+        terminalRone2 = new boolean[rfs.length];
         responses = new Responses(null, null, null);
         rs = new JNIGridnetClient.Response[s1];
     }
@@ -116,9 +132,15 @@ public class JNIGridnetVecClient {
     }
 
     public Responses reset(int[] players) throws Exception {
-        for (int i = 0; i < players.length; i++) {
-            rs[i] = clients[i].reset(players[i]);
+        for (int i = 0; i < selfPlayClients.length; i++) {
+            selfPlayClients[i].reset();
+            rs[i*2] = selfPlayClients[i].getResponse(0);
+            rs[i*2+1] = selfPlayClients[i].getResponse(1);
         }
+        for (int i = selfPlayClients.length*2; i < players.length; i++) {
+            rs[i] = clients[i-selfPlayClients.length*2].reset(players[i]);
+        }
+
         for (int i = 0; i < rs.length; i++) {
             observation[i] = rs[i].observation;
             reward[i] = rs[i].reward;
@@ -129,14 +151,49 @@ public class JNIGridnetVecClient {
     }
 
     public Responses gameStep(int[][][] action, int[] players) throws Exception {
-        for (int i = 0; i < players.length; i++) {
+        for (int i = 0; i < selfPlayClients.length; i++) {
+            selfPlayClients[i].gameStep(action[i*2], action[i*2+1]);
+            rs[i*2] = selfPlayClients[i].getResponse(0);
+            rs[i*2+1] = selfPlayClients[i].getResponse(1);
+            envSteps[i*2] += 1;
+            envSteps[i*2+1] += 1;
+            if (rs[i*2].done[0] || envSteps[i*2] >= maxSteps) {
+                for (int j = 0; j < terminalReward1.length; j++) {
+                    terminalReward1[j] = rs[i*2].reward[j];
+                    terminalRone1[j] = rs[i*2].done[j];
+                    terminalReward2[j] = rs[i*2+1].reward[j];
+                    terminalRone2[j] = rs[i*2+1].done[j];
+                }
+
+                selfPlayClients[i].reset();
+                for (int j = 0; j < terminalReward1.length; j++) {
+                    rs[i*2].reward[j] = terminalReward1[j];
+                    rs[i*2].done[j] = terminalRone1[j];
+                    rs[i*2+1].reward[j] = terminalReward2[j];
+                    rs[i*2+1].done[j] = terminalRone2[j];
+                }
+                rs[i*2].done[0] = true;
+                rs[i*2+1].done[0] = true;
+                envSteps[i*2] =0;
+                envSteps[i*2+1] =0;
+            }
+        }
+
+        for (int i = selfPlayClients.length*2; i < players.length; i++) {
             envSteps[i] += 1;
-            rs[i] = clients[i].gameStep(action[i], players[i]);
-            // System.out.println(i);
-            // System.out.println(rs[i].done[0]);
+            rs[i] = clients[i-selfPlayClients.length*2].gameStep(action[i], players[i]);
             if (rs[i].done[0] || envSteps[i] >= maxSteps) {
-                JNIGridnetClient.Response r = clients[i].reset(players[i]);
-                rs[i].observation = r.observation;
+                // TRICKY: note that `clients` already resets the shared `observation`
+                // so we need to set the old reward and done to this response
+                for (int j = 0; j < rs[i].reward.length; j++) {
+                    terminalReward1[j] = rs[i].reward[j];
+                    terminalRone1[j] = rs[i].done[j];
+                }
+                clients[i-selfPlayClients.length*2].reset(players[i]);
+                for (int j = 0; j < rs[i].reward.length; j++) {
+                    rs[i].reward[j] = terminalReward1[j];
+                    rs[i].done[j] = terminalRone1[j];
+                }
                 rs[i].done[0] = true;
                 envSteps[i] = 0;
             }
@@ -152,8 +209,12 @@ public class JNIGridnetVecClient {
 
 
     public int[][][][] getMasks(int player) throws Exception {
-        for (int i = 0; i < masks.length; i++) {
-            masks[i] = clients[i].getMasks(player);
+        for (int i = 0; i < selfPlayClients.length; i++) {
+            masks[i*2] = selfPlayClients[i].getMasks(0);
+            masks[i*2+1] = selfPlayClients[i].getMasks(1);
+        }
+        for (int i = selfPlayClients.length*2; i < masks.length; i++) {
+            masks[i] = clients[i-selfPlayClients.length*2].getMasks(player);
         }
         return masks;
     }
