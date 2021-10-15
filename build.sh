fc90ec3f7f4108886660b93f727662738fb8f319
Costa Huang <costa.huang@outlook.com>
2021-10-15 18:16:18 -0400

    add a build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+rm -rf build microrts.jar
+mkdir build
+javac -d "./build" -cp "./lib/*" -sourcepath "./src"  $(find ./src/* | grep .java)
+# javac ./src/tests/*.java ./src/tests/sockets/*.java
+cp -a lib/. build/
+cd build
+for i in *.jar; do
+    echo "adding dependency $i"
+    jar xf $i
+done
+jar cvf microrts.jar *
+mv microrts.jar ../microrts.jar
+cd ..
+rm -rf build
