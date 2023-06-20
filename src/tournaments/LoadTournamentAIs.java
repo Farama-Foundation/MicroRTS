ac424c48abb04ba7cd6e45a96c6bab7f61cd918d
DennisSoemers <d.soemers@gmail.com>
2023-06-20 11:28:58 +0200

    Error handling when loading AIs from jars.

--- src/tournaments/LoadTournamentAIs.java
+++ src/tournaments/LoadTournamentAIs.java
@@ -5,8 +5,8 @@
  */
 package tournaments;
 
-import ai.core.AIWithComputationBudget;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import ai.core.AIWithComputationBudget;
 
 /**
  *
@@ -37,7 +39,7 @@ public class LoadTournamentAIs {
     }
     
     
-    public static List<Class> loadTournamentAIsFromJAR(String jarPath) throws Exception {
+    public static List<Class> loadTournamentAIsFromJAR(String jarPath) throws IOException, ClassNotFoundException {
         ClassLoader loader = URLClassLoader.newInstance(new URL[]{new File(jarPath).toURI().toURL()},
                 LoadTournamentAIs.class.getClassLoader()
         );
@@ -51,9 +53,16 @@ public class LoadTournamentAIs {
               break;
             String name = e.getName();
             if (name.endsWith(".class")) {
-                Class c = loader.loadClass(name.substring(0, name.length() - 6).replace('/', '.'));
-                if (!Modifier.isAbstract(c.getModifiers()) && 
-                    isTournamentAIClass(c)) cs.add(c);
+            	try {
+            		Class<?> c = loader.loadClass(name.substring(0, name.length() - 6).replace('/', '.'));
+	                if (!Modifier.isAbstract(c.getModifiers()) && 
+	                    isTournamentAIClass(c)) cs.add(c);
+            	} catch (final NoClassDefFoundError error) {
+            		System.err.println("Skipping class which could not be loaded: " + name);
+            		if (name.equals("org/jdom/xpath/JaxenXPath$NSContext.class")) {
+            			System.err.println("NOTE: this is a common error when packaging all of MicroRTS inside the .jar file with your bot(s)!");
+            		}
+            	}
             }
           }
         return cs;        
